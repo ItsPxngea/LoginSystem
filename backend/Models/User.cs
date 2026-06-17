863de41813f592db8cb3d40d63adf9b21f06f120
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-17 13:51:10 +0200

    Added AuthResponse to user model for authentication purposes when logging in and registering as a user

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.DataProtection;
+using System.Text.Json.Serialization;
 
 namespace backend.Models
 {
@@ -8,20 +8,36 @@ namespace backend.Models
         public string userLastName { get; set; } = string.Empty;
         public string userProfileName { get; set; } = string.Empty;
         public Guid userID { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string passwordHash { get; set; } = string.Empty;
         //private string userPassword { get; set; } = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
-        public string email {get;set;} = string.Empty;
+        public string email { get; set; } = string.Empty;
 
-        public ICollection<RefreshToken> RefreshTokens {get;set;}= new List<RefreshToken>();
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
 
     }
 
     public class UserLogin
     {
-        public string Email {get;set;} = string.Empty;
-        public string Password {get;set;}=string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 
+    public class AuthResponse
+    {
+        public string token { get; set; } = string.Empty;
+        public string expiresAt { get; set; } = string.Empty;
+        public UserDataTransfer user { get; set; } = new();
+    }
+
+    public class UserDataTransfer
+    {
+        public Guid userID { get; set; }
+        public string userFirstName { get; set; } = string.Empty;
+        public string userLastName { get; set; } = string.Empty;
+        public string userProfileName { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+    }
     /*public class Role
     {
         public int ID {get;set;}
