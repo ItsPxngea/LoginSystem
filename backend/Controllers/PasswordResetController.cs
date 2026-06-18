c27bfc3ecbda198c4c329715acb663b3124f845d
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-18 13:05:40 +0200

    Created Controller class for Password resets

---
+++ backend/Controllers/PasswordResetController.cs
@@ -0,0 +1,39 @@
+using backend.Models;
+using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PasswordController : ControllerBase
+    {
+        private readonly IPasswordResetService _service;
+
+        public PasswordController(IPasswordResetService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("forgot")]
+        public async Task<IActionResult> ForgotPassword([FromBody]ForgotPasswordRequest request)
+        {
+            await _service.ForgotPassword(request);
+
+            return Ok(new {message = "A reset link has been sent to the email address"});
+        }
+
+        [HttpPost("reset")]
+        public async Task<IActionResult>ResetPassword([FromBody]PasswordResetRequest request)
+        {
+            try{
+                await _service.ResetPassword(request);
+                return Ok(new{message = "Password has been reset successfully"});
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new {message = e.Message});
+            }
+        }
+    }
+}
