067c85bbc21526dc08665163317d1bae0ad9922f
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-22 20:26:32 +0200

    Added wrapper for google login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
+const GOOGLE_CLIENT_ID = "984293335569-c2eajqc4t7p0e5m7ct1s9744n582gn2g.apps.googleusercontent.com";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
