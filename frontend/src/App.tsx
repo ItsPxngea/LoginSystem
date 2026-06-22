7a3e19756080c3486df19bda95a2046935eb432d
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-22 15:22:27 +0200

    Minor change, modified trailing Route tag

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path ="/register" element={<SignUpPage/>}></Route>
+        <Route path="/register" element={<SignUpPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
