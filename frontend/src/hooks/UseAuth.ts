fd1dbed71eb0210b188e78425e0ce0d0949db398
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-22 15:00:33 +0200

    Called methods for Registration and login via Http requests modified so errors return as well

--- frontend/src/hooks/UseAuth.ts
+++ frontend/src/hooks/UseAuth.ts
@@ -1,36 +1,67 @@
 import { useState } from 'react'
 import { authApi } from '../API/AuthApi'
-import type { User, LoginRequest } from '../types/Auth'
+import type { LoginRequest, RegisterRequest, AuthResponse, UserDTO, ApiError } from '../types/Auth'
 
 export function useAuth() {
-  const [user, setUser] = useState<User | null>(null)
+  //const [user, setUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
 
-  const login = async (credentials: LoginRequest) => {
+  const login = async (credentials: LoginRequest): Promise<boolean> => {
     setLoading(true)
     setError(null)
     try {
-      const { token, user } = await authApi.login(credentials)
-      localStorage.setItem('token', token)
-      setUser(user)
+      //const { token, user } = await authApi.login(credentials)
+      const data = await authApi.login(credentials)
+      localStorage.setItem("user", JSON.stringify(data.user))
+      localStorage.setItem('token', data.token)
+
+      //setUser(user)
       return true
+
     } catch (err: unknown) {
-      const msg =
+      /*const msg =
         err && typeof err === 'object' && 'message' in err
           ? String((err as { message: unknown }).message)
           : 'Login failed'
-      setError(msg)
+      setError(msg)*/
+
+      setError((err as ApiError).message ?? "Login failed. Please try again.")
       return false
+
     } finally {
       setLoading(false)
     }
   }
 
-  const logout = () => {
-    localStorage.removeItem('token')
-    setUser(null)
+  const register = async (data: RegisterRequest): Promise<boolean> => {
+    setLoading(true);
+    setError(null);
+    try {
+
+      const res = await authApi.register(data);
+      localStorage.setItem("token", res.token);
+      localStorage.setItem("user", JSON.stringify(res.user));
+
+      return true;
+
+    } catch (err) {
+
+      setError((err as ApiError).message ?? "Registration failed. Please try again.")
+      return false;
+
+    } finally {
+      setLoading(false)
+    }
   }
 
-  return { user, loading, error, login, logout }
+
+  const logout = () => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
+  }
+
+
+
+  return { login, register, logout, loading, error }
 }
