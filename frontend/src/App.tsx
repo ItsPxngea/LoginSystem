16217dc95ffad7b585a6315642c145d4898b519b
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-24 13:57:45 +0200

    Enabled basic redirection logic to stop users from being able to login to the dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,22 @@ import './index.css'
 import SignUpPage from './pages/SignUp'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
+import ProtectedRoute from './Components/ProtectedRoute'
+import PublicRoute from './Components/PublicRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><SignUpPage /></PublicRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/forgot-password" element={<PublicRoute><ForgotPasswordPage /></PublicRoute>} />
+        <Route path="/reset-password" element={<PublicRoute><ResetPasswordPage /></PublicRoute>} />
+
+        <Route path='*' element={<Navigate to = "/login" replace/>}/>
+
       </Routes>
     </BrowserRouter>
   )
