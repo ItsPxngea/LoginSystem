9cebc73443b4efa800691313b7e7a58dd047a5e3
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-24 13:41:32 +0200

    corrected syntax for decoding token

--- frontend/src/Components/PublicRoute.tsx
+++ frontend/src/Components/PublicRoute.tsx
@@ -11,7 +11,7 @@ function getToken(): string | null {
 
 function isTokenExpired(token: string): boolean {
     try {
-        const payload = JSON.parse(atob("token".split(".")[1]))
+        const payload = JSON.parse(atob(token.split(".")[1]))
 
         if (!payload.exp) return false;
 
