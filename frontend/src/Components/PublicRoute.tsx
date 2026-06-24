a85d636c6adad05790d01d665f497b2a73b15e78
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-24 13:39:25 +0200

    Created a class which checks if token is expired and if not, it should ignore login page and take you straight to dashboard

---
+++ frontend/src/Components/PublicRoute.tsx
@@ -0,0 +1,40 @@
+import { Navigate } from "react-router-dom";
+import type { ReactNode } from "react";
+
+interface ProtectedRouteProps {
+    children: ReactNode;
+}
+
+function getToken(): string | null {
+    return localStorage.getItem("token") ?? sessionStorage.getItem("token")
+}
+
+function isTokenExpired(token: string): boolean {
+    try {
+        const payload = JSON.parse(atob("token".split(".")[1]))
+
+        if (!payload.exp) return false;
+
+        const expiryMs = payload.exp * 1000;
+
+        return Date.now() >= expiryMs;
+
+    } catch {
+        return true;
+    }
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+    const token = getToken();
+
+    if (!token || isTokenExpired(token)) {
+        localStorage.removeItem("token")
+        localStorage.removeItem("user")
+
+        sessionStorage.removeItem("token")
+        sessionStorage.removeItem("user")
+
+        return <Navigate to="/login" replace />
+    }
+    return <>{children}</>
+}
