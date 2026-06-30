8b61496233952226ca572fb9db678cfc19b289fe
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-06-30 14:15:46 +0200

    Profile http requests to communicate between backend and frontend

---
+++ frontend/src/API/ProfileApi.ts
@@ -0,0 +1,8 @@
+import { http } from './Client'
+import type { UpdateUsernameRequest, UpdatePasswordRequest, ProfileResponse } from '../types/Profile'
+
+export const profileApi = {
+    getProfile: () => http.get<ProfileResponse>("/user/profile"),
+    updateUsername: (data: UpdateUsernameRequest) => http.put<{ message: string; userProfileName: string }>("user/username", data),
+    updatePassword: (data: UpdatePasswordRequest) => http.put<{ message: string; }>("user/password", data)
+}
