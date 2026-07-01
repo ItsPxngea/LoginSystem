d96ff20ab0b87daca00ea34fffcc49d03470f4e9
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-07-01 11:41:31 +0200

    Request for verifying password for frontend

--- frontend/src/API/ProfileApi.ts
+++ frontend/src/API/ProfileApi.ts
@@ -4,5 +4,7 @@ import type { UpdateUsernameRequest, UpdatePasswordRequest, ProfileResponse } fr
 export const profileApi = {
     getProfile: () => http.get<ProfileResponse>("/user/profile"),
     updateUsername: (data: UpdateUsernameRequest) => http.put<{ message: string; userProfileName: string }>("user/username", data),
-    updatePassword: (data: UpdatePasswordRequest) => http.put<{ message: string; }>("user/password", data)
+    updatePassword: (data: UpdatePasswordRequest) => http.put<{ message: string; }>("user/password", data),
+    verifyPassword: (currentPassword: string) => http.post<{ message: string }>("/user/verify-password", { currentPassword })
 }
+
