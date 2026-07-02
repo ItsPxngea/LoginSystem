b79bafb601554a3b2bab86a7908a35c9ab147f4f
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-07-02 14:56:46 +0200

    Called errorhandling.css script and wrapped whole application in errorboundary tag to catch any unhandled errors while navigating through the application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,23 +8,31 @@ import ResetPasswordPage from './pages/ResetPasswordPage'
 import ProtectedRoute from './Components/ProtectedRoute'
 import PublicRoute from './Components/PublicRoute'
 import ProfilePage from './pages/ProfilePage'
+import ErrorBoundary from './Components/ErrorBoundary'
+import "./Styles/ErrorHandling.css"
+import OfflineBanner from './Components/OfflineBanner'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
-        <Route path="/register" element={<PublicRoute><SignUpPage /></PublicRoute>} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/forgot-password" element={<PublicRoute><ForgotPasswordPage /></PublicRoute>} />
-        <Route path="/reset-password" element={<PublicRoute><ResetPasswordPage /></PublicRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><ProfilePage/></ProtectedRoute>}/>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <OfflineBanner />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+          <Route path="/register" element={<PublicRoute><SignUpPage /></PublicRoute>} />
 
-        <Route path='*' element={<Navigate to = "/login" replace/>}/>
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
 
-      </Routes>
-    </BrowserRouter>
+          <Route path="/forgot-password" element={<PublicRoute><ForgotPasswordPage /></PublicRoute>} />
+          <Route path="/reset-password" element={<PublicRoute><ResetPasswordPage /></PublicRoute>} />
+
+          <Route path='*' element={<Navigate to="/login" replace />} />
+
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   )
 }
 
