87f79846f32c7a10802b2068aa97c38a339d863d
ItsPxngea <88384372+ItsPxngea@users.noreply.github.com>
2026-07-02 20:57:51 +0200

    Added interface for skeleton loading screen

--- frontend/src/types/Layout.ts
+++ frontend/src/types/Layout.ts
@@ -3,3 +3,10 @@ import type { ReactNode } from "react";
 export interface LayoutProps{
     children:ReactNode
 }
+
+export interface SkeletonProps{
+    width?:string
+    height?:string
+    borderRadius?:string
+    className?:string
+}
